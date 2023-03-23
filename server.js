df636c1ce08e3adfa39829659f15fffa219b727a
Syahir Suhairi <77099451+mdsyrz@users.noreply.github.com>
2023-03-23 15:29:29 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const path = require("path");
 const logger = require("morgan");
-
 require("dotenv").config();
 require("./config/database");
 
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,18 +1,3 @@
-import { useState } from "react";
-import "./App.css";
-
-fetch("/api")
-  .then((response) => response.json())
-  .then((data) => console.log(data));
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className="App">
-      <h1>MERN</h1>
-    </div>
-  );
+export default function App() {
+  return <main className="App">App</main>;
 }
-
-export default App;
